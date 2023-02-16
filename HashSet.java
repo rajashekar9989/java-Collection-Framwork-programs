4eef3bd2e45c82977861550731439821941fe1cd
RAJASHEKAR REDDY <rajas9162@gmail.com>
2023-02-16 18:56:56 +0530

    First commit

---
+++ HashSet.java
@@ -0,0 +1,26 @@
+package examples;
+
+import java.util.ArrayList;
+
+public class HashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList a = new ArrayList();
+		
+		for(int i =0;i<100;i++) {
+			
+			
+				System.out.println(i);
+				
+
+			
+			
+		}
+		
+		System.out.println("hello");
+
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,24 @@
+package examples;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int arr []= {1,2,3,4,5,6};
+	int sum=0;
+		
+		for(int x :arr) {
+			
+		     sum =sum+x;
+		     
+		     
+						
+		}
+		
+		System.out.println("Array sum "+sum);
+
+	}
+
+}
